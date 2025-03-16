3e257d6b7627d9458cbb6b095442d81b9cae94aa
rostislav <rostilosfl@gmail.com>
2025-03-16 23:41:26 +0200

    M1.1 rule refactoring

--- src/main/java/org/perspectiveteam/sonarrules/php/checks/FunctionArgumentsShouldNotBeModifiedCheck.java
+++ src/main/java/org/perspectiveteam/sonarrules/php/checks/FunctionArgumentsShouldNotBeModifiedCheck.java
@@ -1,59 +1,116 @@
 package org.perspectiveteam.sonarrules.php.checks;
 
+import org.sonar.check.Priority;
 import org.sonar.check.Rule;
-import org.sonar.plugins.php.api.tree.SeparatedList;
 import org.sonar.plugins.php.api.tree.Tree;
+import org.sonar.plugins.php.api.tree.declaration.FunctionDeclarationTree;
 import org.sonar.plugins.php.api.tree.declaration.MethodDeclarationTree;
 import org.sonar.plugins.php.api.tree.declaration.ParameterTree;
 import org.sonar.plugins.php.api.tree.expression.AssignmentExpressionTree;
-import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
-import org.sonar.plugins.php.api.tree.expression.IdentifierTree;
+import org.sonar.plugins.php.api.tree.expression.VariableIdentifierTree;
+import org.sonar.plugins.php.api.tree.statement.ForEachStatementTree;
 import org.sonar.plugins.php.api.visitors.PHPVisitorCheck;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Rule(
         key = org.perspectiveteam.sonarrules.php.checks.FunctionArgumentsShouldNotBeModifiedCheck.KEY,
         name = org.perspectiveteam.sonarrules.php.checks.FunctionArgumentsShouldNotBeModifiedCheck.MESSAGE,
-        tags = {"convention"}
+        description = "Function arguments should not be modified within the function body. Use local variables instead.",
+        priority = Priority.MAJOR,
+        tags = {"magento2", "convention", "pitfall", "maintainability"}
 )
-
 public class FunctionArgumentsShouldNotBeModifiedCheck extends PHPVisitorCheck {
 
     public static final String KEY = "M1.1";
     public static final String MESSAGE = "Function arguments SHOULD NOT be modified.";
 
     @Override
-    public void visitAssignmentExpression(AssignmentExpressionTree tree) {
-        ExpressionTree variable = tree.variable();
-        if(variable instanceof IdentifierTree){
-            IdentifierTree identifier = (IdentifierTree) variable;
-            Tree parent = identifier.getParent();
+    public void visitMethodDeclaration(MethodDeclarationTree tree) {
 
-            while (parent != null) {
-                if (parent.is(Tree.Kind.METHOD_DECLARATION)) {
-                    MethodDeclarationTree methodDeclarationTree = (MethodDeclarationTree) parent;
-                    String identifierName = identifier.token().toString();
-                    boolean isArgModified = checkIfFunctionArgumentIsModified(identifier, methodDeclarationTree);
-                    if(isArgModified){
-                        context().newIssue(this, tree.variable(), "Function argument '" + identifierName + "' should not be modified.");
-                    }
-                    break;
-                }
-                parent = parent.getParent();
-            }
-        }
-        super.visitAssignmentExpression(tree);
+        checkFunctionParameters(tree.parameters().parameters(), tree.body());
+
+        super.visitMethodDeclaration(tree);
     }
 
-    private boolean checkIfFunctionArgumentIsModified(IdentifierTree identifier, MethodDeclarationTree methodDeclarationTree) {
-        SeparatedList<ParameterTree> parameters = methodDeclarationTree.parameters().parameters();
+    @Override
+    public void visitFunctionDeclaration(FunctionDeclarationTree tree) {
+        checkFunctionParameters(tree.parameters().parameters(), tree.body());
+        super.visitFunctionDeclaration(tree);
+    }
 
-        for (ParameterTree param : parameters) {
-            if (param.referenceToken() != null) {
-                param.variableIdentifier();
-                return param.variableIdentifier().toString().equals(identifier.token().toString());
-            }
+    private void checkFunctionParameters(Iterable<ParameterTree> parameters, Tree functionBody) {
+        Set<String> parameterNames = new HashSet<>();
+        for (ParameterTree parameter : parameters) {
+            parameterNames.add(parameter.variableIdentifier().text());
         }
-        return false;
+
+        if (parameterNames.isEmpty()) {
+            return;
+        }
+
+        ParameterModificationCollector collector = new ParameterModificationCollector(parameterNames);
+        functionBody.accept(collector);
+        for (IssueInfo issue : collector.getIssues()) {
+            context().newIssue(this, issue.tree, issue.message);
+        }
+    }
+
+    private static class IssueInfo {
+        final Tree tree;
+        final String message;
+
+        IssueInfo(Tree tree, String message) {
+            this.tree = tree;
+            this.message = message;
+        }
+    }
+
+    private static class ParameterModificationCollector extends PHPVisitorCheck {
+        private final Set<String> parameterNames;
+        private final List<IssueInfo> issues = new ArrayList<>();
+
+        ParameterModificationCollector(Set<String> parameterNames) {
+            this.parameterNames = parameterNames;
+        }
+
+        List<IssueInfo> getIssues() {
+            return issues;
+        }
+
+        @Override
+        public void visitAssignmentExpression(AssignmentExpressionTree tree) {
+            if (tree.variable().is(Tree.Kind.VARIABLE_IDENTIFIER)) {
+                VariableIdentifierTree variable = (VariableIdentifierTree) tree.variable();
+                String variableName = variable.text();
+
+                if (!variableName.isEmpty() && parameterNames.contains(variableName)) {
+                    issues.add(new IssueInfo(tree,
+                            "Function argument \"" + variableName + "\" should not be modified. Use a local variable instead."));
+
+                }
+            }
+            super.visitAssignmentExpression(tree);
+        }
+
+        @Override
+        public void visitForEachStatement(ForEachStatementTree tree) {
+            if (tree.value() != null && tree.value().is(Tree.Kind.REFERENCE_VARIABLE)) {
+                if (tree.value().is(Tree.Kind.VARIABLE_IDENTIFIER)) {
+                    VariableIdentifierTree variable = (VariableIdentifierTree) tree.value();
+                    String variableName = variable.text();
+
+                    if (parameterNames.contains(variableName)) {
+                        issues.add(new IssueInfo(tree,
+                                "Function argument \"" + variableName + "\" should not be modified through foreach reference. Use a local variable instead."));
+                    }
+                }
+            }
+            super.visitForEachStatement(tree);
+        }
     }
 }
 
