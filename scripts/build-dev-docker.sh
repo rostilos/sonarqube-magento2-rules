6bdbed68eb6c5eb6ea00781d1bac40dec0fb0d89
rostislav <rostilosfl@gmail.com>
2025-04-16 12:57:21 +0300

    version bump, target to SQ 25.4.0.105899

--- scripts/build-dev-docker.sh
+++ scripts/build-dev-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 PACKAGE_NAME="sonarqube-magento2-rules"
-PACKAGE_VERSION="1.1.0"
-SONARQUBE_VERSION="25.2.0.102705"
+PACKAGE_VERSION="1.2.0"
+SONARQUBE_VERSION="25.4.0.105899"
 INSTANCE_DIR="/var/www/html/sonarqube-instance/docker/"
 BIN_DIR="${INSTANCE_DIR}/bin/linux-x86-64"
 
