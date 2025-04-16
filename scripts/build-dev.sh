91d9345928c2ce2c671c6f1e199946c02a2126cc
rostislav <rostilosfl@gmail.com>
2025-04-16 13:49:33 +0300

    Edit versions, target to version 24.12.0.100206

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGE_NAME="sonarqube-magento2-rules"
+PACKAGE_NAME="sonar-magento2-rules"
 PACKAGE_VERSION="1.2.0"
 SONARQUBE_VERSION="25.4.0.105899"
 INSTANCE_DIR="../sonarqube-instance/native/sonarqube-${SONARQUBE_VERSION}"
@@ -9,5 +9,4 @@ BIN_DIR="${INSTANCE_DIR}/bin/linux-x86-64"
 
 cd ../ && mvn clean package
 cp "./target/${PACKAGE_NAME}-${PACKAGE_VERSION}.jar" "${PLUGIN_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}.jar"
-cp "./target/sonarqube-js-custom-rules-10.0.0.jar" "${PLUGIN_DIR}/sonarqube-js-custom-rules-10.0.0.jar"
 "${BIN_DIR}/sonar.sh" console
